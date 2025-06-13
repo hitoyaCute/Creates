36fa337fc35cc78086d1c3f76105bad23b1110cc
hitoya <nokimberly74@gmail.com>
2025-06-13 21:53:47 +0800

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
+
+
+int main() {
+    
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
-
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
