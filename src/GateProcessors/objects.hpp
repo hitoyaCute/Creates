614d21745477b9a7817ff0742445d027b0279471
hitoya <nokimberly74@gmail.com>
2025-06-14 21:29:01 +0800

    some progress

---
+++ src/GateProcessors/objects.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+
+/* component from 0 to 6
+ * */
+struct gates {
+
+};
+
+struct CustomComponent {
+
+};
+
+/*
+ * int is a type of the component where
+ * it contain the  object types
+ * */
+struct ComponentGroup{
+  std::vector<int> process_group;
+
+
+};
+
+
+
