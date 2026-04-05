87ff1a4329824958babf50060a44087fb6da7f33
hitoya <nokimberly74@gmail.com>
2026-04-05 21:24:33 +0800

    multiple movable objects
    we now have the ability to add dynamic ammount of objects on the screen

--- src/app/interface/scene/main_scene.cpp
+++ src/app/interface/scene/main_scene.cpp
@@ -1,10 +1,12 @@
-#include <SFML/Graphics/Rect.hpp>
-#include <SFML/Window/Keyboard.hpp>
+#include <vector>
 #include <cstdint>
+#include <unordered_map>
 #include <SFML/Window/Mouse.hpp>
+#include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Graphics/Transform.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderStates.hpp>
@@ -13,13 +15,14 @@
 #include <SFML/Graphics/RenderTexture.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 
-
 #include "app/globals.hpp"
 #include "util/sfml_tools.hpp"
 // #include "ui/shapes/GlShapes.hpp"
 #include "ui/shapes/basicShape.hpp"
 #include "app/interface/scene/scene.hpp"
 #include "app/interface/res_handler/Resource.hpp"
+
+
 
 auto font = Resources.get_font("font_1",RES_DIR"fonts/NerdFonts/HackNerdFontMono-Regular.ttf");
 
@@ -37,10 +40,20 @@ sf::Vector2f focus_point {0,0};
 
 // info about interactions
 
-// if the user is dragging
-bool is_dragging;
+// holds the position of the object to be moved, null if there's none
+sf::Vector2f* is_dragging = 0;
+std::string type;
 bool left_down;
 sf::Vector2f dragg_offset;
+
+
+// gate vector map
+// the Gates contains vector of gate's name,position pair
+std::unordered_map<std::string, std::vector<std::pair<std::string,sf::Vector2f>>> Gates;
+
+// temporary buffer
+// this holds the order of types
+std::vector<std::string> buffer;
 
 void draw_main(sf::RenderWindow& window) {
     window.clear(sf::Color {75u,75u,75u});
@@ -49,23 +62,20 @@ void draw_main(sf::RenderWindow& window) {
     const auto mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
     auto local_pos = sfml_utils::current_to_localpos(mousePos, size, focus_point, zoom_level);
 
-    MEU::Shape::CreateCircle(c, quality * 4, 10.f, sf::Vector2f{mousePos}, sf::Color{255,255,0});
-    y_.setString(std::to_string((int)mousePos.y));
-    x_.setString(std::to_string((int)mousePos.x));
-
-    static sf::CircleShape a{50};
-    sf::FloatRect a_bounds = a.getGlobalBounds();
-    a.setFillColor(sf::Color::Blue);
-    
-
     if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-        if (!left_down and !is_dragging and a_bounds.contains(local_pos)) {
-            is_dragging = 1;
-            dragg_offset = a.getPosition() - local_pos;
+        // check which gate is bellow the mouse
+        for (auto& gate: Gates["X"]) {
+            if (is_dragging != 0)
+                break;
+            if (!left_down and !is_dragging and sf::FloatRect{gate.second, {50,50}}.contains(local_pos)) {
+                is_dragging = &gate.second;
+                dragg_offset = gate.second - local_pos;
+                break;
+            }
         }
-        if (is_dragging) {
-            a.setPosition(local_pos + dragg_offset);
-        }
+
+        if (is_dragging != nullptr)
+            *is_dragging = local_pos + dragg_offset;
         left_down = 1;
     } else {
         is_dragging = 0;
@@ -73,30 +83,35 @@ void draw_main(sf::RenderWindow& window) {
     }
     // move the view
     using namespace sf::Keyboard;
-        
-    if (isKeyPressed(Scancode::Left)) {
-        focus_point.x -= 1.5;
-    }
-    if (isKeyPressed(Scancode::Right)) {
-        focus_point.x += 1.5;
-    }
-    if (isKeyPressed(Scancode::Up)) {
-        focus_point.y -= 1.5;
-    }
-    if (isKeyPressed(Scancode::Down)) {
-        focus_point.y += 1.5;
-    }
 
-    sf::RectangleShape d{{10,10}};
+    if (isKeyPressed(Scancode::Left))
+        focus_point.x -= 1.5;
+    if (isKeyPressed(Scancode::Right))
+        focus_point.x += 1.5;
+    if (isKeyPressed(Scancode::Up))
+        focus_point.y -= 1.5;
+    if (isKeyPressed(Scancode::Down))
+        focus_point.y += 1.5;
+
+    MEU::Shape::CreateCircle(c, quality * 4, 10.f, sf::Vector2f{mousePos}, sf::Color{255,255,0});
+    y_.setString(std::to_string((int)mousePos.y));
+    x_.setString(std::to_string((int)mousePos.x));
 
     // drawn at the back
     sf::RenderStates sim_render_state;
     sim_render_state.transform = sfml_utils::get_world_transform(focus_point, size, zoom_level);
+
+    for (const auto& gate: Gates["X"]) {
+        auto d = sf::RectangleShape{{50,50}};
+        d.setPosition(gate.second);
+        window.draw(d, sim_render_state);
+    }
+
+    sf::RectangleShape d{{10,10}};
     window.draw(d, sim_render_state);
 
     d.setPosition(size-sf::Vector2f{10,10});
     window.draw(d, sim_render_state);
-    window.draw(a, sim_render_state);
 
     window.draw(top_shelf);
     window.draw(y_);
@@ -117,5 +132,9 @@ void load_main(sf::RenderWindow& window) {
                                   quality*4,
                                   20.f,
                                   sf::Vector2f{size.x-20.f,100.f},
-                                  sf::Vector2f{10.f,10.f}, sf::Color{200,200,200});  
+                                  sf::Vector2f{10.f,10.f}, sf::Color{200,200,200});
+
+    Gates["X"].emplace_back("meh", sf::Vector2f{100,100});
+    Gates["X"].emplace_back("me2", sf::Vector2f{100,150});
 }
+
